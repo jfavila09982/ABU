87c9917682c6278e9aba0557024773d49b0fbb11
jfavila09982 <jfavila09982@gmail.com>
2024-08-24 12:17:04 +0800

    feat: add endpoint for session controller

--- abu-backend/routes/api.php
+++ abu-backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PollingController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+Route::get('/poll', [PollingController::class, 'poll']);
+
+// Route::get('/test-ip', function () {
+//     return 'whitelisted';
+
+
+//     })->middleware('whitelisted');
+
+
+Route::get('/index', [SessionController::class, 'getUserIp']);    
+Route::post('/create', [SessionController::class, 'createSession']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
