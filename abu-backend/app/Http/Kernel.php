2cf1145cfed950ca05e8e3df8bdbb8465029492a
jfavila09982 <jfavila09982@gmail.com>
2024-08-24 12:15:08 +0800

    feat: add whitelisting middleware

--- abu-backend/app/Http/Kernel.php
+++ abu-backend/app/Http/Kernel.php
@@ -43,6 +43,10 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+
+
+
+
     ];
 
     /**
@@ -64,5 +68,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        //add whitelisted middleware
+        'whitelisted' => \App\Http\Middleware\WhiteListed::class,
     ];
 }
