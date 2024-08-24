e57bac54952a173c30c0f4452b543db1733f2ddc
jfavila09982 <jfavila09982@gmail.com>
2024-08-24 12:15:23 +0800

    feat: dedicated IPWhiteListController

--- abu-backend/app/Http/Controllers/IPWhitelistController.php
+++ abu-backend/app/Http/Controllers/IPWhitelistController.php
@@ -11,13 +11,16 @@ class IPWhitelistController extends Controller
 
         $ips = IPAddress::all();
 
-        $data = ['message' => 'List of IPs Whitelisted',
+        $data = [
+                 'message' => 'List of IPs Whitelisted',
                  'data' => $ips,
                  'statusCode' => 200,
                 ]; 
-                
+
 
         return response()->json($data);
 
     }
+    
 }
+
